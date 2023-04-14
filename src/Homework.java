1682dd012c7490c12d1887926b554bd9385ad10c
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-15 00:54:08 +0200

    Add List students

--- src/Homework.java
+++ src/Homework.java
@@ -16,5 +16,27 @@ public class Homework {
 //   в списке не больше 10 учеников (извините, конкурс)
 // Для обработки и вывода результата используйте потоки (Stream).
 
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    List<Student> students = new ArrayList<>();
+    students.add(new Student("Anton", 3));
+    students.add(new Student("Boris", 2));
+    students.add(new Student("Valera", 4));
+    students.add(new Student("Artem", 5));
+    students.add(new Student("Oleg", 4));
+    students.add(new Student("Pavel", 3));
+    students.add(new Student("Elena", 2));
+    students.add(new Student("Valeria", 4));
+    students.add(new Student("Artemia", 5));
+    students.add(new Student("Olga", 4));
+    students.add(new Student("Andrey", 3));
+    students.add(new Student("Denis", 2));
+    students.add(new Student("Vadim", 4));
+    students.add(new Student("Maksim", 5));
+    students.add(new Student("Egor", 4));
+    System.out.print("Введите проходной балл ");
+    int score=Integer.parseInt(br.readLine());
 
+
+  }
 }
